4ea6719ea72ea37cdd524954fb5007d7e5f810d9
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-11-07 20:07:53 -0700

    functionality increase

--- sznAnalysis.py
+++ sznAnalysis.py
@@ -7,14 +7,14 @@ class player_analysis():
         self.name = name
         self.years_active = []
     
-    def szn_count(self, year=2020, typi='per_game'): #TODO change so multiple analysis modes supported
+    def szn_count(self, year=2020,typi='all'): #TODO change so multiple analysis modes supported
         while True:
             temp = szn_analysis(year)
             try:
-                temp.add_player_structure(self.name, typi)
-
+                temp.add_player_structure(self.name,typi=typi)
             except:
                 break
+            print('as')
             self.years_active.append(year)
             year -= 1
             self.szns.append(temp)
@@ -26,10 +26,21 @@ class szn_analysis():
         self.year = year
         self.nba_request = Requests.nba_request()
     
-    def add_player_structure(self, name, typi='per_game'):
-        stat = self.add_frame(typi=typi)
-        full_set = self.create_structure(stat)
-        self.structures.append(full_set[name])
+    def add_player_structure(self, name, typi='all'):
+        intermediary = []
+        if type(typi) == type('string'):
+            for x in range(0,5):
+                stat = self.add_frame(typi=x)
+                full_set = self.create_structure(stat)
+                intermediary.append(full_set['STATS'])
+                intermediary.append(full_set[name])
+                self.structures.append(intermediary)
+        else:
+            stat = self.add_frame(typi=typi)
+            full_set = self.create_structure(stat)
+            intermediary.append(full_set['STATS'])
+            intermediary.append(full_set[name])
+            self.structures.append(intermediary)
 
 
     def create_structure(self, frame):
@@ -41,17 +52,27 @@ class szn_analysis():
 
         return dic
     
-    def add_frame(self, typi='per_game'):
-        if typi == 'per_game':
+    def add_frame(self, typi=1):
+        if typi == 0:
+            return self.nba_request.totals(self.nba_request.url, self.year)
+        elif typi == 1:
             return self.nba_request.per_game(self.nba_request.url, self.year)
+        elif typi == 2:
+            return self.nba_request.per_36(self.nba_request.url, self.year)
+        elif typi == 3:
+            return self.nba_request.per_100(self.nba_request.url, self.year)
+        elif typi == 4:
+            return self.nba_request.advanced(self.nba_request.url, self.year)
+        else:
+            print('invalid')
 
-# g = szn_analysis(2020)
-# print(g.year)
-# g.add_player_structure('Steven Adams')
-# print(g.structures[0])
 
-g = player_analysis('Steven Adams')
+
+
+
+g = player_analysis('Stephen Curry')
 print(g.name)
-g.szn_count(year=2020, typi='per_game')
+g.szn_count(year=2020, typi=1)
 print(g.years_active)
-print(g.szns[0].structures[0])
+for x in g.szns:
+    print(x.structures[0][1])
