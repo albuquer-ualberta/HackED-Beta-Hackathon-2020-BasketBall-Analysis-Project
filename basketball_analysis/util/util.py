cf8edf327eae1685e0618ea0320844b77bd2407d
Albert Dinh <68311441+devfrearth@users.noreply.github.com>
2020-11-07 22:23:59 -0700

    update util 2nd

--- basketball_analysis/util/util.py
+++ basketball_analysis/util/util.py
@@ -3,31 +3,42 @@
 
 import statistics
 
-# Initial parameters
-# FIXME Potentially will have to take into account other ways to input
-# request_var = Requests.nba_request()
-# season = int(input('Enter a season: '))
-# req = input('Enter the desired mode: ')
 
-# # FIXME Potentially more modes to add
-# if req == 'totals':
-#     table = request_var.totals(request_var.url, year=season)
-# elif req == 'per-game':
-#     table = request_var.per_game(request_var.url, year=season)
+def get_vertical_index(typi, category):
+    """ Take a user input for what datatype and category
 
-# # An object of class
-# season_analysis_obj = sznAnalysis.szn_analysis(table)
+    Index reference from the user
+    0: Totals
+    1: Per-game
+    2: Per-36-Min
+    3: Per-100-Possession
+    4: Advanced
 
-# # A dictionary of players and their statistics
-# structure_obj_szn_analysis = season_analysis_obj.create_stucture(table)
+    :param typi: a type of file
+    :param category: a string input by the user to extract from a file
+    :return:
+    """
+    if typi in [0, 1, 2, 3, 4]:
+        # FIXME Could add more if needed
+        if category == 'AGE':
+            return 2
+        elif category == 'GAMES':
+            return 4
 
-# name_players = list(structure_obj_szn_analysis.keys())[1:]
+
+def get_list_names(season_data):
+    """ Take in a dictionary object (class sznAnalysis)
+    :return:
+        name_list: a list of player names
+    """
+    name_players = list(season_data.keys())[1:]
+
+    return name_players
 
 
-def get_wanted_parameter(full_structure, names, index_para):
+def get_wanted_parameter(season_data, names, index_para):
     """ Take in a list of players' names, a wanted parameter and return
-    :param a_wanted_parameter: a string defined by the user
-    :param full_structure: a dictionary of players' statistics
+    :param season_data: a dictionary of players' statistics
     :param names: a list of players' names
     :param index_para: index of the wanted parameter depending on the file
     :return:
@@ -35,18 +46,19 @@ def get_wanted_parameter(full_structure, names, index_para):
     """
     a_list_of_wanted_para = []
     for name in names:
-        stat_for_player = full_structure[name]
+        stat_for_player = season_data[name]
         para = stat_for_player[index_para]
         a_list_of_wanted_para.append(para)
 
     return a_list_of_wanted_para
 
 
-def compute_median_age(a_para_list):
+def compute_median(a_para_list):
     """Take in a list and compute the median
     :param a_para_list: a list of parameters for all player
     """
-    median_para = statistics.median(a_para_list)
+    num_lt = [float(i) for i in a_para_list]
+    median_para = statistics.median(num_lt)
 
     return median_para
 
@@ -56,8 +68,11 @@ def compute_stdev(a_para_list):
     :param a_para_list: a list of parameters for all player
     :return:
     """
-    std_ev = statistics.stdev(a_para_list)
+    num_lt = [float(i) for i in a_para_list]
+    std_ev = statistics.stdev(num_lt)
+
+    return std_ev
 
 
 if __name__ == '__main__':
-    main()
+    pass
